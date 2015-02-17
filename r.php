3f326f275b0e9f8cef7be94a46ada0589bcf2f70
Max Verbeek <m4xv3rb33k@gmail.com>
2015-02-17 22:19:23 +0100

    Escape the name

--- r.php
+++ r.php
@@ -4,7 +4,7 @@ require_once 'core/init.php';
 header('Content-type: text/JSON');
 
 if(isset($_GET['q'])):
-	$query = $_GET['q'];
+	$query = htmlentities($_GET['q'], ENT_QUOTES, "UTF-8");
 	$movies = Database::inst()->get('movies', 'name = '.$query)->result();
 	echo json_encode($movies);
 endif;
